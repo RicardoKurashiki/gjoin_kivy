844b5f8d88fe9e141eb83e47a9c6d2e6f6658421
carlosqas <65908990+carlosqas@users.noreply.github.com>
2020-05-25 17:36:00 -0300

    Alteração KV
    Adicionando text = '' para adicionar no banco de dados

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.app import App
 import sqlite3
+import re
+
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+def check(email):  
+    conf = 1
+    if(re.search(regex,email)):  
+        conf = 1
+    else:  
+        print('Insira um e-mail válido')
+        conf = 0
+    return conf
 
 class LoginPage(Screen):
     def __init__(self, **kwargs):
@@ -25,30 +36,83 @@ class LoginPage(Screen):
         self.manager.transition.direction = 'up'
         self.manager.current = 'register'
 
+
 class RegisterPage(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
     # Empurra para o DB
     def register_account(self):
-        class Registro(object):
-            def __init__(self, nome, email, universidade, curso, senha, confsenha):
-                self.nome = nome
-                self.email = email
-                self.universidade = universidade
-                self.curso = curso
-                self.senha = senha
-                self.confsenha = confsenha
+        conf = 1
 
-        NovReg = Registro(nome = self.ids.reg_name.text, email = self.ids.reg_email.text, universidade = self.ids.reg_univ.text, curso = self.ids.reg_curso.text, senha = self.ids.reg_pass.text, confsenha = self.ids.reg_confirmPass.text) # Criação do novo registro
-        conn = sqlite3.connect('BANCO.db') # Conexão com o DB
+        # Verificação de email no banco de dados
+        conn = sqlite3.connect('BANCO.db')
         cursor = conn.cursor()
-
-        cursor.execute("""INSERT INTO dados (usuario, email, senha, universidade, curso) VALUES (?,?,?,?,?) """, (NovReg.nome, NovReg.email, NovReg.senha, NovReg.universidade, NovReg.curso))
-
-        conn.commit()
+        cursor.execute("""SELECT email FROM dados;""")
+        for registro in cursor.fetchall():
+            if registro == (self.ids.reg_email.text,):
+                conf = 0
+                print('Digite um email que não esteja em uso')
         conn.close()
-    
+
+        if self.ids.reg_name.text == '':
+            conf = 0
+            print('Insira um nome')
+
+        elif self.ids.reg_email.text == '':
+            conf = 0
+            print('Insira um email')
+        
+        elif self.ids.reg_univ.text == '':
+            conf = 0
+            print('Insira uma universidade')
+
+        elif self.ids.reg_curso.text == '':
+            conf = 0
+            print('Insira um curso')
+
+        elif self.ids.reg_pass.text == '':
+            conf = 0
+            print('Insira uma senha')
+
+        elif self.ids.reg_confirmPass.text ==  '':
+            conf = 0
+            print('Insira uma confirmação de senha')
+
+        elif self.ids.reg_pass.text != self.ids.reg_confirmPass.text:
+            conf = 0
+            print('As senhas digitadas precisam ser iguais!')
+        
+        elif conf == 1 and check(self.ids.reg_email.text) == 1:
+            class Registro(object):
+                def __init__(self, nome, email, universidade, curso, senha, confsenha):
+                    self.nome = nome
+                    self.email = email
+                    self.universidade = universidade
+                    self.curso = curso
+                    self.senha = senha
+                    self.confsenha = confsenha
+
+            NovReg = Registro(nome = self.ids.reg_name.text, email = self.ids.reg_email.text, universidade = self.ids.reg_univ.text, curso = self.ids.reg_curso.text, senha = self.ids.reg_pass.text, confsenha = self.ids.reg_confirmPass.text) # Criação do novo registro
+            conn = sqlite3.connect('BANCO.db') # Conexão com o DB
+            cursor = conn.cursor()
+
+            cursor.execute("""INSERT INTO dados (usuario, email, senha, universidade, curso) VALUES (?,?,?,?,?) """, (NovReg.nome, NovReg.email, NovReg.senha, NovReg.universidade, NovReg.curso))
+
+            conn.commit()
+            print('------------------------------------  Registrado ------------------------------------')
+            conn.close()
+
+            self.manager.transition.direction = 'down'
+            self.manager.current = 'login'
+
+            self.ids.reg_name.text = ''
+            self.ids.reg_email.text = ''
+            self.ids.reg_univ.text = ''
+            self.ids.reg_curso.text = ''
+            self.ids.reg_pass.text = ''
+            self.ids.reg_confirmPass.text = ''
+
     def cancel_register(self):
         self.manager.transition.direction = 'down'
         self.manager.current = 'login'
@@ -58,7 +122,6 @@ class HomePage(Screen):
     imagem_app = StringProperty('gjoinLogo.png')
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
 
     def go_back(self):
         self.manager.transition.direction = 'right'
